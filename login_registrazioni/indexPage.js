5b76b42fdaa9f479c4c07408cb6bd07e55e913f9
Roberta <roberta.cramarossa@gmail.com>
2019-11-08 17:27:28 +0100

    new

---
+++ login_registrazioni/indexPage.js
@@ -0,0 +1,10 @@
+
+    $('#go').click(function(){
+        $("block").animate({
+
+            opacity:0.4,
+            marginLeft:"0.6in",
+            fontSize: "3em",
+            borderWidth: "10px"
+        }, 1500);
+    });
